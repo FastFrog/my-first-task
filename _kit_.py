4d2d637e9441e9a6b4953955141ea2de8fef4fb1
fastfrog <fast.angelin@gmail.com>
2021-10-31 01:08:27 +0600

    changed _kit_.py file

--- _kit_.py
+++ _kit_.py
@@ -1,13 +1,16 @@
 x = float(input("meaning x ="))
 y = float(input("meaning y ="))
 s = input("operator(+,-,/,*)=")
+
 if s == "+":
     print(x+y)
+
 elif s == "-":
     print(x-y)
 
 elif s == "*":
     print(x*y)
+
 elif s == "/":
     if y != 0:
         print(x/y)
